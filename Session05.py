f2876b8299a33788fe5074373882b8c563696b69
Pablo Estevez Arnanz <pablo.estevez@estudiantat.upc.edu>
2026-04-30 16:05:46 +0200

    create session05

---
+++ Session05.py
@@ -0,0 +1,19 @@
+ this is Your Name homework
+
+from typing import cast, Any
+
+
+# DEFINE INPUTS
+x = cast(int, x)  # type: ignore
+y = cast(int, y)  # type: ignore
+
+
+# MAIN CODE
+
+
+
+
+
+
+# DEFINE OUTPUTS
+a = 0
